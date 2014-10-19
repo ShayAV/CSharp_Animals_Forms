65b52847200610f528b57b2f9187aaf1ab9b5f2f
Shay Avrahami <shay640@gmail.com>
2014-10-19 17:10:11 +0300

    Create Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,334 @@
+namespace Animals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AnimalsListBox = new System.Windows.Forms.ListBox();
+            this.DisplayBugsCheckBox = new System.Windows.Forms.CheckBox();
+            this.DisplayFishCheckBox = new System.Windows.Forms.CheckBox();
+            this.DisplayDinosaursCheckBox = new System.Windows.Forms.CheckBox();
+            this.DisplayAnimals = new System.Windows.Forms.Button();
+            this.ShowDetailsButton = new System.Windows.Forms.Button();
+            this.detailsRichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Ebutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.ageRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.weightRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.ageComboBox = new System.Windows.Forms.ComboBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.weightComboBox = new System.Windows.Forms.ComboBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.dangerButton = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AddAnimalButton = new System.Windows.Forms.Button();
+            this.passwordButton = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // AnimalsListBox
+            // 
+            this.AnimalsListBox.FormattingEnabled = true;
+            this.AnimalsListBox.Location = new System.Drawing.Point(217, 27);
+            this.AnimalsListBox.Name = "AnimalsListBox";
+            this.AnimalsListBox.Size = new System.Drawing.Size(142, 225);
+            this.AnimalsListBox.TabIndex = 4;
+            // 
+            // DisplayBugsCheckBox
+            // 
+            this.DisplayBugsCheckBox.AutoSize = true;
+            this.DisplayBugsCheckBox.Location = new System.Drawing.Point(118, 65);
+            this.DisplayBugsCheckBox.Name = "DisplayBugsCheckBox";
+            this.DisplayBugsCheckBox.Size = new System.Drawing.Size(50, 17);
+            this.DisplayBugsCheckBox.TabIndex = 6;
+            this.DisplayBugsCheckBox.Text = "Bugs";
+            this.DisplayBugsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // DisplayFishCheckBox
+            // 
+            this.DisplayFishCheckBox.AutoSize = true;
+            this.DisplayFishCheckBox.Location = new System.Drawing.Point(118, 93);
+            this.DisplayFishCheckBox.Name = "DisplayFishCheckBox";
+            this.DisplayFishCheckBox.Size = new System.Drawing.Size(45, 17);
+            this.DisplayFishCheckBox.TabIndex = 9;
+            this.DisplayFishCheckBox.Text = "Fish";
+            this.DisplayFishCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // DisplayDinosaursCheckBox
+            // 
+            this.DisplayDinosaursCheckBox.AutoSize = true;
+            this.DisplayDinosaursCheckBox.Location = new System.Drawing.Point(118, 116);
+            this.DisplayDinosaursCheckBox.Name = "DisplayDinosaursCheckBox";
+            this.DisplayDinosaursCheckBox.Size = new System.Drawing.Size(73, 17);
+            this.DisplayDinosaursCheckBox.TabIndex = 10;
+            this.DisplayDinosaursCheckBox.Text = "Dinosaurs";
+            this.DisplayDinosaursCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // DisplayAnimals
+            // 
+            this.DisplayAnimals.Location = new System.Drawing.Point(127, 27);
+            this.DisplayAnimals.Name = "DisplayAnimals";
+            this.DisplayAnimals.Size = new System.Drawing.Size(75, 23);
+            this.DisplayAnimals.TabIndex = 11;
+            this.DisplayAnimals.Text = "Display";
+            this.DisplayAnimals.UseVisualStyleBackColor = true;
+            this.DisplayAnimals.Click += new System.EventHandler(this.DisplayAnimals_Click);
+            // 
+            // ShowDetailsButton
+            // 
+            this.ShowDetailsButton.Location = new System.Drawing.Point(462, 27);
+            this.ShowDetailsButton.Name = "ShowDetailsButton";
+            this.ShowDetailsButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowDetailsButton.TabIndex = 13;
+            this.ShowDetailsButton.Text = "Details :";
+            this.ShowDetailsButton.UseVisualStyleBackColor = true;
+            this.ShowDetailsButton.Click += new System.EventHandler(this.ShowDetailsButton_Click);
+            // 
+            // detailsRichTextBox1
+            // 
+            this.detailsRichTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.detailsRichTextBox1.Location = new System.Drawing.Point(543, 29);
+            this.detailsRichTextBox1.Name = "detailsRichTextBox1";
+            this.detailsRichTextBox1.Size = new System.Drawing.Size(374, 154);
+            this.detailsRichTextBox1.TabIndex = 14;
+            this.detailsRichTextBox1.Text = "";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(2, 509);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // Ebutton
+            // 
+            this.Ebutton.Location = new System.Drawing.Point(854, 506);
+            this.Ebutton.Name = "Ebutton";
+            this.Ebutton.Size = new System.Drawing.Size(75, 23);
+            this.Ebutton.TabIndex = 16;
+            this.Ebutton.Text = "Eventbutton";
+            this.Ebutton.UseVisualStyleBackColor = true;
+            this.Ebutton.Click += new System.EventHandler(this.Ebutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(484, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(654, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 18);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Age:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(806, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 18);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Weight:";
+            // 
+            // nameRichTextBox
+            // 
+            this.nameRichTextBox.Location = new System.Drawing.Point(543, 206);
+            this.nameRichTextBox.Name = "nameRichTextBox";
+            this.nameRichTextBox.Size = new System.Drawing.Size(100, 32);
+            this.nameRichTextBox.TabIndex = 23;
+            this.nameRichTextBox.Text = "";
+            // 
+            // ageRichTextBox
+            // 
+            this.ageRichTextBox.Location = new System.Drawing.Point(697, 206);
+            this.ageRichTextBox.Name = "ageRichTextBox";
+            this.ageRichTextBox.Size = new System.Drawing.Size(100, 32);
+            this.ageRichTextBox.TabIndex = 24;
+            this.ageRichTextBox.Text = "";
+            // 
+            // weightRichTextBox
+            // 
+            this.weightRichTextBox.Location = new System.Drawing.Point(870, 206);
+            this.weightRichTextBox.Name = "weightRichTextBox";
+            this.weightRichTextBox.Size = new System.Drawing.Size(100, 32);
+            this.weightRichTextBox.TabIndex = 25;
+            this.weightRichTextBox.Text = "";
+            // 
+            // ageComboBox
+            // 
+            this.ageComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ageComboBox.FormattingEnabled = true;
+            this.ageComboBox.Location = new System.Drawing.Point(697, 271);
+            this.ageComboBox.Name = "ageComboBox";
+            this.ageComboBox.Size = new System.Drawing.Size(100, 21);
+            this.ageComboBox.TabIndex = 26;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(557, 271);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 27;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // weightComboBox
+            // 
+            this.weightComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.weightComboBox.FormattingEnabled = true;
+            this.weightComboBox.Location = new System.Drawing.Point(870, 271);
+            this.weightComboBox.Name = "weightComboBox";
+            this.weightComboBox.Size = new System.Drawing.Size(99, 21);
+            this.weightComboBox.TabIndex = 28;
+            // 
+            // dangerButton
+            // 
+            this.dangerButton.Location = new System.Drawing.Point(748, 506);
+            this.dangerButton.Name = "dangerButton";
+            this.dangerButton.Size = new System.Drawing.Size(75, 23);
+            this.dangerButton.TabIndex = 30;
+            this.dangerButton.Text = "Danger!";
+            this.dangerButton.UseVisualStyleBackColor = true;
+            this.dangerButton.Click += new System.EventHandler(this.dangerButton_Click_1);
+            this.dangerButton.MouseHover += new System.EventHandler(this.MaskButtonHover);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.BeepOnError = true;
+            this.maskedTextBox1.Location = new System.Drawing.Point(869, 399);
+            this.maskedTextBox1.Mask = "00000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 31;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(694, 402);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 13);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Don\'t enter more than 5 digits";
+            // 
+            // AddAnimalButton
+            // 
+            this.AddAnimalButton.Location = new System.Drawing.Point(127, 299);
+            this.AddAnimalButton.Name = "AddAnimalButton";
+            this.AddAnimalButton.Size = new System.Drawing.Size(75, 23);
+            this.AddAnimalButton.TabIndex = 37;
+            this.AddAnimalButton.Text = "Add Animal";
+            this.AddAnimalButton.UseVisualStyleBackColor = true;
+            this.AddAnimalButton.Click += new System.EventHandler(this.AddAnimalButton_Click);
+            // 
+            // passwordButton
+            // 
+            this.passwordButton.Location = new System.Drawing.Point(639, 506);
+            this.passwordButton.Name = "passwordButton";
+            this.passwordButton.Size = new System.Drawing.Size(75, 23);
+            this.passwordButton.TabIndex = 38;
+            this.passwordButton.Text = "Password";
+            this.passwordButton.UseVisualStyleBackColor = true;
+            this.passwordButton.Click += new System.EventHandler(this.passwordButton_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(376, 372);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(121, 97);
+            this.treeView1.TabIndex = 39;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 588);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.passwordButton);
+            this.Controls.Add(this.AddAnimalButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dangerButton);
+            this.Controls.Add(this.weightComboBox);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.ageComboBox);
+            this.Controls.Add(this.weightRichTextBox);
+            this.Controls.Add(this.ageRichTextBox);
+            this.Controls.Add(this.nameRichTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ebutton);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.detailsRichTextBox1);
+            this.Controls.Add(this.ShowDetailsButton);
+            this.Controls.Add(this.DisplayAnimals);
+            this.Controls.Add(this.DisplayDinosaursCheckBox);
+            this.Controls.Add(this.DisplayFishCheckBox);
+            this.Controls.Add(this.DisplayBugsCheckBox);
+            this.Controls.Add(this.AnimalsListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox AnimalsListBox;
+        private System.Windows.Forms.CheckBox DisplayBugsCheckBox;
+        private System.Windows.Forms.CheckBox DisplayFishCheckBox;
+        private System.Windows.Forms.CheckBox DisplayDinosaursCheckBox;
+        private System.Windows.Forms.Button DisplayAnimals;
+        private System.Windows.Forms.Button ShowDetailsButton;
+        private System.Windows.Forms.RichTextBox detailsRichTextBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button Ebutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox nameRichTextBox;
+        private System.Windows.Forms.RichTextBox ageRichTextBox;
+        private System.Windows.Forms.RichTextBox weightRichTextBox;
+        private System.Windows.Forms.ComboBox ageComboBox;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.ComboBox weightComboBox;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button dangerButton;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button AddAnimalButton;
+        private System.Windows.Forms.Button passwordButton;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
